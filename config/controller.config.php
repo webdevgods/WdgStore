b59e6ec0d3845d5045df04e5ea48be2fa3123bd5
dustinmoorman <dustin.moorman@gmail.com>
2014-04-04 13:34:23 -0500

    Starting controller setup

---
+++ config/controller.config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'invokables' => array(
+        'WdgBlog\Controller\Store' => 'WdgStore\Controller\StoreController',
+        'WdgBlog\Controller\StoreAdmin' => 'WdgStore\Controller\AdminController',
+        'WdgBlog\Controller\StoreAdminProduct' => 'WdgStore\Controller\AdminProductController',
+        'WdgBlog\Controller\StoreAdminCategory' => 'WdgStore\Controller\AdminCategoryController',
+    ),
+);
