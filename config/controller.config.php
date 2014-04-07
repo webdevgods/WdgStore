ac0977164766e57b8537b61fc2825ce9aacb0328
Dustin Moorman <dustin.moorman@gmail.com>
2014-04-06 21:36:41 -0500

    Adding subscription controller to config.

--- config/controller.config.php
+++ config/controller.config.php
@@ -4,6 +4,7 @@ return array(
         'WdgStore\Controller\Store' => 'WdgStore\Controller\StoreController',
         'WdgStore\Controller\StoreAdmin' => 'WdgStore\Controller\StoreAdminController',
         'WdgStore\Controller\StoreAdminProduct' => 'WdgStore\Controller\StoreAdminProductController',
+        'WdgStore\Controller\StoreAdminSubscription' => 'WdgStore\Controller\StoreAdminSubscriptionController',
         'WdgStore\Controller\StoreAdminCategory' => 'WdgStore\Controller\StoreAdminCategoryController',
     ),
 );
