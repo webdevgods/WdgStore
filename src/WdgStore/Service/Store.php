cf6a65b5dcbd037d38d61ae41d0097bc6d4fd286
Dustin Moorman <dustin.moorman@gmail.com>
2014-04-06 22:14:13 -0500

    Initial Store Service commit.

---
+++ src/WdgStore/Service/Store.php
@@ -0,0 +1,12 @@
+<?php
+namespace WdgStore\Service;
+
+use Zend\Form\Form,
+    WdgZf2\Service\ServiceAbstract,
+    WdgStore\Entity\Product as PostEntity,
+    WdgStore\Entity\Category as CategoryEntity;
+
+class Store extends ServiceAbstract
+{
+
+}
