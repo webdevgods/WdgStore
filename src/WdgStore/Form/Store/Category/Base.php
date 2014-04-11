7b74c06dea2e6e72b65dc598a829177c89135e24
Dustin Moorman <dustin.moorman@gmail.com>
2014-04-10 21:15:50 -0500

    Adding Base Category Form

---
+++ src/WdgStore/Form/Store/Category/Base.php
@@ -0,0 +1,26 @@
+<?php
+namespace WdgStore\Form\Store\Category;
+
+use WdgZf2\Form\PostFormAbstract;
+
+class Base extends PostFormAbstract
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->add(array(
+            'name' => 'name',
+            'options' => array(
+                'label' => 'Name',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'slug',
+            'options' => array(
+                'label' => 'Slug',
+            ),
+        ));
+    }
+}
