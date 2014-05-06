740a7e5f653b31f2e48a3c0d6ce55117d15040a0
Jessie Green <jessie.ims@gmail.com>
2014-05-05 21:25:54 -0500

    Store product views

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,12 @@
 <?php
 return array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public',
+            ),
+        ),
+    ),
     'doctrine' => array(
         'driver' => array(
             'WdgStore_driver' => array(
--- src/WdgStore/Controller/StoreController.php
+++ src/WdgStore/Controller/StoreController.php
@@ -6,12 +6,41 @@ use Zend\View\Model\ViewModel;
 
 class StoreController extends AbstractActionController
 {
+    protected $service;
+    
     public function indexAction()
     {
-        /* @var $store_service \WdgStore\Service\Store */
-        $store_service   = $this->getServiceLocator()->get('wdgstore_service_store');
-        $featured_products = $store_service->getFeaturedProductsPaginator(1, 5);
+        $store_service      = $this->getStoreService();
+        $featured_products  = $store_service->getFeaturedProductsPaginator(1, 5);
         
         return new ViewModel(array("featured_products" => $featured_products));
+    }
+    
+    public function productAction()
+    {
+        $slug       = $this->params()->fromRoute('slug', '');
+        $service    = $this->getStoreService();
+        $product    = $service->getProductBySlug($slug);
+        
+        if(!$product)
+        {
+            $this->getResponse()->setStatusCode(404);
+            return; 
+        }
+        
+        return new ViewModel(array("product" => $product));
+    }
+    
+    /**
+     * @return \WdgStore\Service\Store
+     */
+    public function getStoreService()
+    {
+        if($this->service === null)
+        {
+            $this->service = $this->getServiceLocator()->get('wdgstore_service_store');
+        }
+        
+        return $this->service;
     }
 }
